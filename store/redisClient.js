c0e9fa3fc24b63bc7b73530b424c2097233c0ecd
Minh Nguyen <xiuquay55@gmail.com>
2022-04-12 23:41:24 +0700

    modify code

--- store/redisClient.js
+++ store/redisClient.js
@@ -27,9 +27,27 @@ function RedisClient(params = {}) {
         chalk.green.bold(`Func setSetex has been start with key:${key}, seconds:${seconds}, value:${value}`)
       );
 
-      return client.setEx(key, seconds, value);
+      await client.connect();
+
+      client.setEx(key, seconds, value, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis setEx has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis setEx successfully with`, {
+            requestId: `${requestId}`,
+            args: reply,
+          });
+        }
+      });
+
+      loggerFactory.info(`Func setSetex has been end`, {
+        requestId: `${requestId}`,
+      });
     } catch (err) {
-      loggerFactory.error(`Func setSetex has been error : ${err}`, {
+      loggerFactory.error(`Func setSetex has been error: ${err}`, {
         requestId: `${requestId}`,
       });
       return Promise.reject(err);
@@ -46,13 +64,29 @@ function RedisClient(params = {}) {
       loggerFactory.info(`Func setOne has been start`, {
         requestId: `${requestId}`,
       });
-      loggerTracer.info(chalk.green(`Func setOne has been start with ${key}-${value}`));
+      loggerTracer.info(chalk.green.bold(`Func setOne has been start with ${key}-${value}`));
+      await client.connect();
 
-      return client.set(key, value);
-    } catch (err) {
-      loggerFactory.error(`Func setOne has been error : ${err}`, {
+      client.set(key, value, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis set has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis set successfully with`, {
+            requestId: `${requestId}`,
+            args: reply,
+          });
+        }
+      });
+
+      loggerFactory.info(`Func setOne has been end`, {
         requestId: `${requestId}`,
-        args: { err },
+      });
+    } catch (err) {
+      loggerFactory.error(`Func setOne has been error: ${err}`, {
+        requestId: `${requestId}`,
       });
       return Promise.reject(err);
     }
@@ -67,13 +101,76 @@ function RedisClient(params = {}) {
       loggerFactory.info(`Func getOne has been start`, {
         requestId: `${requestId}`,
       });
-      loggerTracer.info(chalk.green(`Func getOne has been start with ${key}`));
+      loggerTracer.info(chalk.green.bold(`Func getOne has been start with ${key}`));
 
-      return client.get(key);
-    } catch (err) {
-      loggerFactory.error(`Func getOne has been error : ${err}`, {
+      let result;
+
+      await client.connect();
+
+      client.get(key, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis get has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis get successfully!`, {
+            requestId: `${requestId}`,
+          });
+          result = reply;
+        }
+      });
+
+      loggerFactory.info(`Func getOne has been end`, {
         requestId: `${requestId}`,
-        args: { err },
+      });
+
+      return result;
+    } catch (err) {
+      loggerFactory.error(`Func getOne has been error: ${err}`, {
+        requestId: `${requestId}`,
+      });
+      return Promise.reject(err);
+    }
+  };
+
+  /**
+   * Get all key from redis store
+   * @param {*} pattern
+   */
+  this.findAll = async function ({ pattern = '*' }) {
+    try {
+      loggerFactory.info(`Func findAll has been start`, {
+        requestId: `${requestId}`,
+      });
+      loggerTracer.info(chalk.green.bold(`Func findAll has been start with ${pattern}`));
+
+      let result;
+
+      await client.connect();
+
+      client.keys(pattern, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis keys has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis keys successfully!`, {
+            requestId: `${requestId}`,
+          });
+          result = reply;
+        }
+      });
+
+      loggerFactory.info(`Func findAll has been end`, {
+        requestId: `${requestId}`,
+      });
+
+      return result;
+    } catch (err) {
+      loggerFactory.error(`Func findAll has been error: ${err}`, {
+        requestId: `${requestId}`,
       });
       return Promise.reject(err);
     }
@@ -85,18 +182,33 @@ function RedisClient(params = {}) {
    * @param {*} field
    * @param {*} value
    */
-  this.setHashes = function ({ key, field, value }) {
+  this.setHashes = async function ({ key, field, value }) {
     try {
       loggerFactory.info(`Func setHashes has been start`, {
         requestId: `${requestId}`,
       });
-      loggerTracer.info(chalk.green(`Func setHashes has been start with ${key}-${field}-${value}`));
+      loggerTracer.info(
+        chalk.green.bold(`Func setHashes has been start with key:${key}-field:${field}-value:${value}`)
+      );
 
-      return client.hSet(key, field, value);
+      await client.connect();
+
+      client.hSet(key, field, value, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis setHashes has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis setHashes successfully with`, {
+            requestId: `${requestId}`,
+            args: reply,
+          });
+        }
+      });
     } catch (err) {
-      loggerFactory.error(`Func setHashes has been error : ${err}`, {
+      loggerFactory.error(`Func setHashes has been error: ${err}`, {
         requestId: `${requestId}`,
-        args: { err },
       });
       return Promise.reject(err);
     }
@@ -111,34 +223,100 @@ function RedisClient(params = {}) {
       loggerFactory.info(`Func getHashes has been start`, {
         requestId: `${requestId}`,
       });
-      loggerTracer.info(chalk.green(`Func getHashes has been start with ${key}`));
+      loggerTracer.info(chalk.green.bold(`Func getHashes has been start with ${key}`));
 
-      return client.hGetAll(key);
-    } catch (err) {
-      loggerFactory.error(`Func getHashes has been error : ${err}`, {
+      let result;
+
+      await client.connect();
+
+      client.hGetAll(key, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis getHashes has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis getHashes successfully!`, {
+            requestId: `${requestId}`,
+          });
+          result = reply;
+        }
+      });
+
+      loggerFactory.info(`Func getHashes has been end`, {
         requestId: `${requestId}`,
-        args: { err },
+      });
+
+      return result;
+    } catch (err) {
+      loggerFactory.error(`Func getHashes has been error: ${err}`, {
+        requestId: `${requestId}`,
       });
       return Promise.reject(err);
     }
   };
 
   /**
-   * Delete key value from redis store
+   * Delete one from redis store
    * @param {*} key
    */
-  this.deleteAll = async function ({ key }) {
+  this.deleteOne = function ({ key }) {
+    try {
+      loggerFactory.info(`Func deleteOne has been start`, {
+        requestId: `${requestId}`,
+      });
+      loggerTracer.info(chalk.green.bold(`Func deleteOne has been start with ${key}`));
+
+      client.del(key, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis del has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis del successfully with`, {
+            requestId: `${requestId}`,
+            args: reply,
+          });
+        }
+      });
+    } catch (err) {
+      loggerFactory.error(`Func deleteOne has been error: ${err}`, {
+        requestId: `${requestId}`,
+      });
+      return Promise.reject(err);
+    }
+  };
+
+  /**
+   * Delete all key from redis store
+   * @param {*} mode
+   */
+  this.deleteAll = async function ({ mode }) {
     try {
       loggerFactory.info(`Func deleteAll has been start`, {
         requestId: `${requestId}`,
       });
-      loggerTracer.info(chalk.green(`Func deleteAll has been start with ${key}`));
+      loggerTracer.info(chalk.green.bold(`Func deleteAll has been start with ${mode}`));
 
-      return client.del(key);
+      await client.connect();
+
+      return client.flushAll(mode, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis setHashes has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis setHashes successfully with`, {
+            requestId: `${requestId}`,
+            args: reply,
+          });
+        }
+      });
     } catch (err) {
-      loggerFactory.error(`Func deleteAll has been error : ${err}`, {
+      loggerFactory.error(`Func deleteAll has been error: ${err}`, {
         requestId: `${requestId}`,
-        args: { err },
       });
       return Promise.reject(err);
     }
@@ -154,13 +332,26 @@ function RedisClient(params = {}) {
       loggerFactory.info(`Func createOrPushIntoTop has been start`, {
         requestId: `${requestId}`,
       });
-      loggerTracer.info(chalk.green(`Func createOrPushIntoTop has been start with ${key}-${value}`));
+      loggerTracer.info(chalk.green.bold(`Func createOrPushIntoTop has been start with key:${key}-value:${value}`));
 
-      return client.lPush(key, value);
+      await client.connect();
+
+      client.lPush(key, value, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis lPush has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis lPush successfully with`, {
+            requestId: `${requestId}`,
+            args: reply,
+          });
+        }
+      });
     } catch (err) {
-      loggerFactory.error(`Func createOrPushIntoTop has been error : ${err}`, {
+      loggerFactory.error(`Func createOrPushIntoTop has been error: ${err}`, {
         requestId: `${requestId}`,
-        args: { err },
       });
       return Promise.reject(err);
     }
@@ -178,11 +369,24 @@ function RedisClient(params = {}) {
       });
       loggerTracer.info(chalk.green(`Func pushIntoBottom has been start with ${key}-${value}`));
 
-      return client.rPush(key, value);
+      await client.connect();
+
+      client.rPush(key, value, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis rPush has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis rPush successfully with`, {
+            requestId: `${requestId}`,
+            args: reply,
+          });
+        }
+      });
     } catch (err) {
-      loggerFactory.error(`Func pushIntoBottom has been error : ${err}`, {
+      loggerFactory.error(`Func pushIntoBottom has been error: ${err}`, {
         requestId: `${requestId}`,
-        args: { err },
       });
       return Promise.reject(err);
     }
@@ -192,40 +396,34 @@ function RedisClient(params = {}) {
    * List all the items in a list using from redis store
    * @param {*} key
    */
-  this.findAll = async function ({ key }) {
+  this.findList = async function ({ key }) {
     try {
-      loggerFactory.info(`Func findAll has been start`, {
+      loggerFactory.info(`Func findList has been start`, {
         requestId: `${requestId}`,
       });
-      loggerTracer.info(chalk.green(`Func findAll has been start with ${key}`));
+      loggerTracer.info(chalk.green(`Func findList has been start with ${key}`));
 
-      const result = await client.lRange(key, 0, -1);
+      await client.connect();
+
+      let result;
+
+      client.lRange(key, 0, -1, (err, reply) => {
+        if (err) {
+          loggerFactory.error(`redis lRange has been error`, {
+            requestId: `${requestId}`,
+            args: err,
+          });
+        } else {
+          loggerFactory.warn(`redis lRange successfully!`, {
+            requestId: `${requestId}`,
+          });
+          result = reply;
+        }
+      });
       return result;
     } catch (err) {
-      loggerFactory.error(`Func findAll has been error : ${err}`, {
+      loggerFactory.error(`Func findList has been error: ${err}`, {
         requestId: `${requestId}`,
-        args: { err },
-      });
-      return Promise.reject(err);
-    }
-  };
-
-  /**
-   * Drop items from a list using using from redis store
-   * @param {*} key
-   */
-  this.deleteOne = function ({ key }) {
-    try {
-      loggerFactory.info(`Func deleteOne has been start`, {
-        requestId: `${requestId}`,
-      });
-      loggerTracer.info(chalk.green(`Func deleteOne has been start with ${key}`));
-
-      return client.rPop(key);
-    } catch (err) {
-      loggerFactory.error(`Func deleteOne has been error : ${err}`, {
-        requestId: `${requestId}`,
-        args: { err },
       });
       return Promise.reject(err);
     }
